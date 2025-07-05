2793c5b144b441bf73edd2aacdb93b8b89858e96
cyanostudios <marionasr@gmail.com>
2025-07-05 13:15:30 +0200

    Fresh start v2: Clean project structure

--- client/src/components/layout/modules/MiddleColumn.tsx
+++
@@ -1,9 +0,0 @@
-import { MainContentArea } from "../main-content-area";
-
-export function MiddleColumn() {
-  return (
-    <div className="middle-column flex-1 bg-neutral-50">
-      <MainContentArea />
-    </div>
-  );
-}
--- client/src/pages/calendar.tsx
+++
@@ -1,9 +0,0 @@
-import InvoiceCalendar from '@/components/calendar/invoice-calendar';
-
-export default function Calendar() {
-  return (
-    <div className="h-full">
-      <InvoiceCalendar />
-    </div>
-  );
-}
