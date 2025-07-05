4c0d69e691faa6416eb7df1b3c46a43bdee9956b
cyanostudios <marionasr@gmail.com>
2025-07-05 13:27:26 +0200

    Basic frontend + backend working with proxy

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,18 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    runtimeErrorOverlay(),
-    ...(process.env.NODE_ENV !== "production" &&
-    process.env.REPL_ID !== undefined
-      ? [
-          await import("@replit/vite-plugin-cartographer").then((m) =>
-            m.cartographer(),
-          ),
-        ]
-      : []),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(process.cwd(), "client", "src"),
-      "@shared": path.resolve(process.cwd(), "shared"),
-      "@assets": path.resolve(process.cwd(), "attached_assets"),
+      '@': path.resolve(__dirname, './client/src'),
     },
   },
-  root: path.resolve(process.cwd(), "client"),
-  build: {
-    outDir: path.resolve(process.cwd(), "dist/public"),
-    emptyOutDir: true,
-  },
-});
+  server: {
+    port: 3000,
+    proxy: {
+      '/api': 'http://localhost:3001'
+    }
+  }
+})
